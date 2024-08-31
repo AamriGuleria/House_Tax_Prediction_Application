22c1f3fd7bd439b860c5403130f346b3727933e1
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2024-09-01 00:16:34 +0530

    Final changes and enhancements

--- app.py
+++ app.py
@@ -2,14 +2,6 @@ from flask import Flask,render_template, request, jsonify
 import pickle
 import os
 import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import GridSearchCV
 
 path = os.path.join(os.path.dirname(__file__), 'house_tax_model.pkl')
 scaler = os.path.join(os.path.dirname(__file__), 'scaler.pkl')
@@ -39,13 +31,13 @@ def predict():
                     float(data['lstat']),float(data['medv'])]
 #         crim	zn	indus	chas	nox	rm	age	dis	rad	tax	ptratio	b	lstat	medv
 # 0	0.00632	18.0	2.31	0	0.538	6.575	65.2	4.0900	1	296	15.3	396.90	4.98	24.0
-        # Convert features to a numpy array and reshape for the model
+        # Convert features to a numpy array and reshape it to an array with one row and as many columns as possible
         features = np.array(features).reshape(1, -1)
         scaled_features = scale.transform(features)
         prediction = model.predict(scaled_features)
         return jsonify({'prediction': prediction[0]})
     
-    except ValueError as e:
-        return jsonify({'error': str(e)})
+    except Exception as e:
+        return jsonify({'error': e})
 if __name__ == '__main__':
     app.run(debug=True)
